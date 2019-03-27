901a356683b538e4c1e791d0b55945615a9e9e59
mstamenkov <bul2013@abv.bg>
2019-03-27 15:43:18 +0200

    added login page AGAIN

---
+++ public/script.js
@@ -0,0 +1,19 @@
+function field_focus(field, email){
+            if(field.value == email){
+              field.value = '';
+            }
+          }
+
+          function field_blur(field, email){
+            if(field.value == ''){
+              field.value = email;
+            }
+          }
+
+        $(document).ready(function(){
+            $('.box').hide().fadeIn(1000);
+            });
+
+        $('a').click(function(event){
+            event.preventDefault(); 
+            });
