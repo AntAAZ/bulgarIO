ad1f88e2a64ae4cd7b55b46cb596390c1c6b7f98
AntoniiZ <AntoniiZ@abv.bg>
2019-04-09 13:17:37 +0300

    Added dynamic food spawning + divided different stuff into files

--- server.js
+++ server.js
@@ -1,131 +1,125 @@
-/// TO:DO -> move classes/global constants into other files
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mysql = require('mysql');
-var path = require('path');
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let path = require("path");
+const config = require('./config');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "bulgar_io"
-});
+const mapSize = config.mapSize,
+    foodSize = config.food.size,
+    foodColor = config.food.color,
+    playerColor = config.player.color,
+    playerSize = config.player.size,
+    foodsAmount = config.food.amount;
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-//   con.query("CREATE DATABASE bulgar_io", function (err, result) {
-//    if (err) throw err;
-//    console.log("Database created");
-//  });
-  
-//   var sql = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))";
-//  con.query(sql, function (err, result) {
-//    if (err) throw err;
-//    console.log("Table created");
-//  });
-
-
-  
-});
-
-app.set('port', 3000);
-app.set('views', path.join(__dirname, 'public'));
-
-app.use(express.static('public'));
-
-var bloops = new Map(),
+let bloops = new Map(),
     leaderboard = new Map(),
-    foods = [],
-    mapSize = 25000;
+    foods = [];
 
-for (let i = 0; i < mapSize; i++) {
+/// routes
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/main/index.html'));
+});
+app.get('/game.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/main/game.js'));
+});
+app.get('/events/events.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/events/events.js'));
+});
+app.get('/api/objects.js', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/api/objects.js'));
+});
 
-    foods.push({
+function objectInit(colorProperty) {
+    let gameObject = {
         'x': -mapSize + Math.random() * mapSize * 2,
         'y': -mapSize + Math.random() * mapSize * 2,
-        'color': [100 + Math.random() * 156, 100 + Math.random() * 156, 200 + Math.random() * 56]
-    });
+        'radius': colorProperty === playerColor ? playerSize : foodSize,
+        'color': [colorProperty.red[0] + Math.random() * (colorProperty.red[1] - colorProperty.red[0]),
+            colorProperty.green[0] + Math.random() * (colorProperty.green[1] - colorProperty.green[0]),
+            colorProperty.blue[0] + Math.random() * (colorProperty.blue[1] - colorProperty.blue[0])]
+    }
+    gameObject.posx = gameObject.x;
+    gameObject.posy = gameObject.y;
+    return gameObject;
 }
 
-io.on('connection', function(socket) {
+for (let i = 0; i < foodsAmount; i++) {
+    foods.push(objectInit(foodColor));
+}
+
+io.on('connection', function (socket) {
 
     console.log(`ID ${socket.id} connected!`);
-    socket.emit('init', foods);
 
-    leaderboard.forEach(function(value, key) {
+    socket.emit('init', {
+        'foods': foods,
+        'mapSize': mapSize,
+        'playerSize': playerSize
+    });
+
+    leaderboard.forEach(function (value, key) {
         socket.emit('leaderboard', {
             'id': key,
             'score': value
         });
     });
 
-    socket.on('update', function(data) {
-        if (data.id != undefined) {
-            bloops.set(data.id, {
-                'id': data.id,
-                'x': data.x,
-                'y': data.y,
-                'posx': data.posx,
-                'posy': data.posy,
-                'color': data.color,
-                'radius': data.radius
-            });
-            socket.broadcast.emit('update', data);
-        }
+    socket.on('spawn', function () {
+        let bloop = objectInit(playerColor);
+        bloop.id = socket.id;
+        bloops.set(bloop.id, bloop);
+        leaderboard.set(bloop.id, bloop.radius);
+        io.emit('spawn', bloop);
+    })
+
+    socket.on('update', function (data) {
+        bloops.set(data.id, {
+            'id': data.id,
+            'x': data.x,
+            'y': data.y,
+            'posx': data.posx,
+            'posy': data.posy,
+            'radius': data.radius,
+            'color': data.color
+        });
+        socket.broadcast.emit('update', data);
     });
-    socket.on('leaderboard', function(score) {
+    socket.on('leaderboard', function (score) {
         leaderboard.set(socket.id, score);
 
-        socket.broadcast.emit('leaderboard', {
+        io.emit('leaderboard', {
             'id': socket.id,
             'score': score
         });
     });
-    socket.on('eat', function(data) {
-        if (data.id != undefined) {
-            console.log(`ID ${data.id} was eaten! Respawning...`);
-
-            let bloop = {
-                'x': -mapSize + Math.random() * mapSize * 2,
-                'y': -mapSize + Math.random() * mapSize * 2,
-                'radius': 50,
-                'color': data.color,
-                'id': data.id
-            };
-
-            bloop.posx = bloop.x;
-            bloop.posy = bloop.y;
-
-            io.emit('update', bloop);
-        }
+    socket.on('eat', function (data) {
+        let bloop = objectInit(playerColor);
+        bloop.id = data.id;
+        bloops.set(bloop.id, bloop);
+        leaderboard.set(bloop.id, bloop.radius);
+        io.emit('spawn', bloop);
     });
-    socket.on('removeFood', function(data) {
-        if (data.index != undefined) {
-            foods.splice(data.index, 1);
+    socket.on('updateFood', function (data) {
+        foods[data.index] = objectInit(foodColor);
 
-            //console.log(`ID ${socket.id} grew up by eating food!`);
-
-            socket.broadcast.emit('removeFood', data);
-        }
+        io.emit('updateFood', {
+            'index': data.index,
+            'initializer': foods[data.index]
+        });
     });
 
-    socket.on('disconnect', function() {
-
+    socket.on('disconnect', function () {
         bloops.delete(socket.id);
         leaderboard.delete(socket.id);
-
-        console.log(`ID ${socket.id} disconnected!`);
-
         socket.broadcast.emit('delete', {
             'id': socket.id
         });
+        console.log(`ID ${socket.id} disconnected!`);
     });
 });
 
 
-http.listen(3000, function() {
+http.listen(config.serverPort, function () {
     console.log("server started on port 3000");
 });
